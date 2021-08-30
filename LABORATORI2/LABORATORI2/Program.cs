60e582a5170c9f52f46748e84c491b6b45d20c7c
victorernesto <88003634+victorernesto@users.noreply.github.com>
2021-08-30 17:52:54 -0600

    cambio

--- LABORATORI2/LABORATORI2/Program.cs
+++ LABORATORI2/LABORATORI2/Program.cs
@@ -10,6 +10,20 @@ namespace LABORATORI2
     {
         static void Main(string[] args)
         {
+            clsPersona clspersona = new clsPersona();
+            Persona persona = new Persona();
+            //ClsPersona clsPersona = new ClsPersona();
+            Console.WriteLine("Escriba los siguientes datos" +
+                "\n\n Escribe tu nombre :");
+            String nombre = Console.ReadLine();
+            Console.WriteLine("Ahora edad: ");
+            int edad = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el sexo  : ");
+            string sexo = Console.ReadLine();
+            Console.WriteLine("Ingresa la altura : ");
+            string altura = Console.ReadLine();
+
+            ClsPersona clsPersona = new ClsPersona(nombre, edad, sexo);
         }
     }
 }
