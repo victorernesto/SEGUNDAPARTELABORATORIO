57a249495f22b2918b846dcbafdd91697f5b9e74
manuelcastro92 <88003360+notavailable231niesomevalido1345w46@users.noreply.github.com>
2021-08-30 16:30:59 -0600

    cambio2

--- LABORATORI2/LABORATORI2/Negocios/Clspersona.cs
+++ LABORATORI2/LABORATORI2/Negocios/Clspersona.cs
@@ -8,5 +8,36 @@ namespace LABORATORI2.Negocios
 {
     class Clspersona
     {
-    }
+
+        ClsPersona persona = new ClsPersona();
+        public string calcularIMC()
+        {
+
+
+            double pro = persona.Peso / Math.Pow(persona.Altura, 2);
+
+
+            if (pro < 20)
+            {
+                return "peso es ideal";
+            }
+            if (pro > 25)
+            {
+                return "tiene sobre peso";
+            }
+            
+
+
+        }
+        public string esMayorDeEdad()
+        {
+            if (persona.Edad >= 18)
+            {
+                return "Es mayor de edad";
+            }
+            else
+                return "No es mayor de edad";
+
+
+        }
 }
